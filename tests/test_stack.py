426849b7020ce83bc5c62ff8150b5a0f64efbb33
Kris <ssspers92@gmail.com>
2024-01-17 23:19:58 +0300

    Change from assertEquals to assertEqual

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -8,18 +8,18 @@ class TestStack(unittest.TestCase):
     def test_node(self):
         node_1 = Node('a', 2)
 
-        self.assertEquals(Node(5, None).data, 5)
-        self.assertEquals(Node(5, node_1).next_node, node_1)
+        self.assertEqual(Node(5, None).data, 5)
+        self.assertEqual(Node(5, node_1).next_node, node_1)
 
     def test_stack(self):
         stack = Stack()
         stack.push('data')
 
-        self.assertEquals(stack.top.data, 'data')
+        self.assertEqual(stack.top.data, 'data')
 
         stack.push('data2')
-        self.assertEquals(stack.top.data, 'data2')
-        self.assertEquals(stack.top.next_node.data, 'data')
+        self.assertEqual(stack.top.data, 'data2')
+        self.assertEqual(stack.top.next_node.data, 'data')
 
     def test_stack_pop(self):
         stack = Stack()
@@ -28,7 +28,7 @@ class TestStack(unittest.TestCase):
 
         deleted_data = stack.pop()
 
-        self.assertEquals(deleted_data, 'data2')
-        self.assertEquals(stack.top.data, 'data')
-        self.assertEquals(len(stack.stack), 1)
+        self.assertEqual(deleted_data, 'data2')
+        self.assertEqual(stack.top.data, 'data')
+        self.assertEqual(len(stack.stack), 1)
 
