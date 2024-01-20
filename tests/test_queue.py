e0e1a7ad851ce8c7b4da5dfadad8b2215cf568e7
Kris <ssspers92@gmail.com>
2024-01-20 17:26:12 +0300

    create test_dequeue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -13,3 +13,28 @@ class TestQueue(unittest.TestCase):
         queue.enqueue('data3')
 
         self.assertEqual(str(queue), "data1\ndata2\ndata3")
+
+    def test_enqueue(self):
+        queue = Queue()
+
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+
+        self.assertEqual(queue.head.data, 'data1')
+        self.assertEqual(queue.head.next_node.data, 'data2')
+        self.assertEqual(queue.head.next_node.next_node.data, 'data3')
+        self.assertEqual(queue.tail.data, 'data3')
+        self.assertEqual(queue.tail.next_node, None)
+
+    def test_dequeue(self):
+        queue = Queue()
+
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+
+        self.assertEqual(queue.dequeue(), 'data1')
+        self.assertEqual(queue.dequeue(), 'data2')
+        self.assertEqual(queue.dequeue(), 'data3')
+        self.assertEqual(queue.dequeue(), None)
