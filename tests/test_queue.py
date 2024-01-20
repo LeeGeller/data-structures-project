de066cadbfd3a3f857ec9f21f0a9cb0e5720db90
Kris <ssspers92@gmail.com>
2024-01-20 17:05:29 +0300

    create test_str

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,15 @@
-"""Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+
+from src.queue import Queue
+
+
+class TestQueue(unittest.TestCase):
+
+    def test_str(self):
+        queue = Queue()
+
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+
+        self.assertEqual(str(queue), "data1\ndata2\ndata3")
